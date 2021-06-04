a2666632913986318b5fd500167732f47ab95ed9
Chris Usick <christopher.usick@gmail.com>
2021-06-04 09:30:45 -0500

    saving progress

---
+++ README.md
@@ -0,0 +1,9 @@
+# Envoy filter render page
+
+test with 
+```
+make build-image
+make build
+docker-compose up --build
+curl localhost:8080
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,26 @@
 use proxy_wasm::types::Action;
 use proxy_wasm::traits::HttpContext;
 use proxy_wasm::traits::Context;
-use log::trace;
+use log::info;
+
+#[no_mangle]
+pub fn _start() {
+    proxy_wasm::set_log_level(proxy_wasm::types::LogLevel::Trace);
+    proxy_wasm::set_http_context(|context_id, _| -> Box<dyn HttpContext> {
+        Box::new(RenderPageFilter { context_id })
+    });
+}
 
 struct RenderPageFilter {
     context_id: u32,
-    template: String
+    // template: &str
 }
 
 impl Context for RenderPageFilter {}
 
 impl HttpContext for RenderPageFilter {
     fn on_http_request_headers(&mut self, _: usize) -> Action {
-        trace!("In WASM : #{}", self.context_id);
+        info!("In WASM : #{}", self.context_id);
         self.send_http_response(
             403,
             vec![("Powered-By", "proxy-wasm")],
